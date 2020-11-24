703dd77d352f672597dc048dd200fbd2ecb6997d
Ciljhon <jhonnycesilio@gmail.com>
2020-11-23 22:47:27 -0400

    La segunda parte de vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,66 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Inicios</router-link> ||
-    <router-link to="/about">A serca de nosotros</router-link>||
-    <router-link to="/actividad">Actividades</router-link>||
-    <router-link to="/contacto">Contactenos</router-link>||
-    <router-link to="/blog">Nuestro Blog</router-link>||
-    <router-link to="/tienda">Nuestra Tienda</router-link>
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <a class="navbar-brand" href="#"><h1> NOMB. EMPRESA</h1></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">INICIO <span class="sr-only">(current)</span></router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about"> QUIENES SOMOS</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" to="/actividad">ACTIVIDADES</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" to="/contacto">CONTACTANOS</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" to="/blog">BLOG</router-link>
+      </li>
+      
+      <li class="nav-item">
+        <router-link class="nav-link" to="/tienda">IR A TIENDA</router-link>
+      </li>
+      
+
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar:...</button>
+    </form>
   </div>
+</nav>
+
+<div class="container">
   <router-view/>
+</div>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,5 +1,66 @@
 <template>
   <h1>{{titulo}}</h1>
+
+  <div class="row">
+    <div class="col-md-4">
+      <h1>Nueva Actiividad</h1>
+      <form >
+        <label for="">Ingrese Titulo</label>
+        <input type="text" class="form-control" v-model="act.titulo">
+
+        <label for="">Ingrese Detalle</label>
+        <textarea class="form-control" v-model="act.detalle"></textarea>
+        <br>
+        <button type="button" class="btn btn-success float-right" @click="guardarAct()">Guardar Actividad</button>
+      </form>
+      {{act}}
+    </div>
+
+    <div class="col-md-8">
+      <h1>List de Actividades</h1>
+      <table class="table table-striperd table-hover">
+        <tr>
+          <td>ID</td>
+          <td>NOMBRE</td>
+          <td>DETALLE</td>
+          <td>ACCIONES</td>
+        </tr>
+
+        <tr v-for="(actividad, index) in actividades" :key="index">
+          <td>{{index}}</td>
+          <td>{{actividad.titulo}}</td>
+          <td>{{actividad.detalle}}</td>
+          <td>
+        <button class="btn btn-outline-primary btn-xs" @click="enproceso(actividad)">EMPEZAR</button>
+        <button class="btn btn-danger btn-xs" @click="eliminarAct(index)">ELIMINAR</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+   <br>
+
+    <div class="row ">
+      <div  class="col-md-4 bg-warning text-light">
+        <h3>EN PROCESO</h3>
+      <div class="card bg-danger" v-for="(a, index) in actividades" :key="index">
+        <div class="titulo" v-if="a.estado === 1">
+          <h1>{{a.titulo}}</h1>
+        </div>
+      </div>
+    </div>
+      <div  class="col-md-4 bg-success text-light"><h3>FINALIZADO</h3>
+      </div>
+      <div  class="col-md-4 bg-danger text-light"><h3>CANCELADO</h3>
+      </div>
+    </div>
+
+ 
+
+  
+<br>
+
+
   <h4>Cantidad de Actividades:{{numero}}</h4>
   <input type="text" v-model="numero">
   <button @click="aumentar">+</button>
@@ -11,6 +72,7 @@
   </ol>
 </template>
 
+
 <script>
 export default {
   data(){
@@ -18,7 +80,8 @@ export default {
       titulo: "ACTIVIDADES",
       numero: 14,
       nombre:["Juan","Cristisn","jhonnyce","jael"],
-
+      actividades:[],
+      act:{titulo:"",detalle:"", estado:0}
     }
   },
   methods:{
@@ -28,11 +91,29 @@ export default {
     },
     reducir(){
       this.numero--;
-
+    },
+      guardarAct(){
+        var aux=this.act
+        this.reset();
+        this.actividades.push(aux)
+      },
+      reset(){
+         this.act = {titulo:"", detalle:""}
+      },
+      eliminarAct(posicion){
+        this.actividades.splice(posicion, 1);
+      },
+      enproceso(actividad){
+        actividad.estado = 1;
+      },
+      finalizado(actividad){
+        actividad.estado = 2;
+      },
+      cancelado(actividad){
+        actividad.estado = 3;
+      }
     }
   }
-
-}
 </script>
 
 <style>
